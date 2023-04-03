e1fcc8f6613e90a09c43d74341a47542184646aa
girish-thetechieguy <girish.thetechieguy@gmail.com>
2023-04-03 21:38:50 +0800

    added the unit test cases and logs

--- src/main/java/com/simplilearn/demo/achistartech/AchistarTechApplication.java
+++ src/main/java/com/simplilearn/demo/achistartech/AchistarTechApplication.java
@@ -1,20 +1,32 @@
 package com.simplilearn.demo.achistartech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
+
 @SpringBootApplication
 @RestController
 public class AchistarTechApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(AchistarTechApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(AchistarTechApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init(){
+		logger.info("Application started......");
+	}
+
 	@RequestMapping("/")
 	public String homepage(){
+		logger.info("Application starting......");
 		return "Hi all! Welcome to Simplilearn assignment!";
 	}
 }
--- src/test/java/com/simplilearn/demo/achistartech/AchistarTechApplicationTests.java
+++ src/test/java/com/simplilearn/demo/achistartech/AchistarTechApplicationTests.java
@@ -1,13 +1,21 @@
 package com.simplilearn.demo.achistartech;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AchistarTechApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    public static Logger logger = LoggerFactory.getLogger(AchistarTechApplicationTests.class);
+	@Test
+	void contextLoads() {
+        logger.info("TestCase is executing......");
+        assertEquals(true,true);
+    }
+
 
 }
